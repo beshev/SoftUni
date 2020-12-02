2dd28580f82a440a89bb2f112f1f286e210c89fe
beshev <beshev1@gmail.com>
2020-12-02 21:59:10 +0200

    Added homework to from Programing Basics

---
+++ Programing Basics/SimpleOparation/03.CalculatingAFaceOfSquaring/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Calculating_a_face_of_squaring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int sum = a * a;
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ Programing Basics/SimpleOparation/10.ZooMagazin/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ZooMagazin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dogs = int.Parse(Console.ReadLine());
+            int animals = int.Parse(Console.ReadLine());
+            double sum = (dogs * 2.50) + (animals * 4);
+
+            Console.WriteLine($"{sum} lv.");
+        }
+    }
+}
