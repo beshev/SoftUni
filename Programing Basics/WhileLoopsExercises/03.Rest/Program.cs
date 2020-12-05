0618dd8bf15560da94df9bd521a3a046a1ac4166
beshev <beshev1@gmail.com>
2020-12-05 13:45:48 +0200

    Added homework from course Programing Basics

---
+++ Programing Basics/WhileLoopsExercises/03.Rest/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace _03.Rest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read Input. 
+            double priceVacantoin = double.Parse(Console.ReadLine());
+            double moneyHave = double.Parse(Console.ReadLine());
+            int daysGoneBy = 0;
+            int daysSpend = 0;
+            string action = "";
+            double sumSpenOrSave = 0;
+
+            // Find whether She -> Save or Spen Money
+            while ((moneyHave < priceVacantoin) && (daysSpend < 5))
+            {
+                daysGoneBy++;
+                action = Console.ReadLine();
+                sumSpenOrSave = double.Parse(Console.ReadLine());
+                // Find money spend
+               if (action == "spend")
+                {
+                    daysSpend += 1;
+                    moneyHave -= sumSpenOrSave;
+                    if (moneyHave < 0)
+                    {
+                        moneyHave = 0;
+                    }
+                }
+                // Find money save 
+                if (action == "save")
+                {
+                    moneyHave += sumSpenOrSave;
+                    daysSpend = 0;
+                }
+
+            }
+                // Find she have money for vacantion or Days Spend == 5;
+                if (moneyHave >= priceVacantoin)
+                {
+                    Console.WriteLine($"You saved the money for {daysGoneBy} days.");
+                   
+                }
+                if (daysSpend == 5)
+                {
+                    Console.WriteLine($"You can't save the money.");
+                    Console.WriteLine($"{daysGoneBy}");
+          
+                }
+
+        }
+    }
+}
---
+++ Programing Basics/WhileLoopsExercises/04.Steps/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _04.Steps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read Inut
+            int stepsWalk = 0;
+            string step = Console.ReadLine();
+            while (step != "Going home")
+            {
+                stepsWalk += int.Parse(step);
+                // Find if we reach the Target
+                if (stepsWalk >= 10000)
+                {
+                    Console.WriteLine("Goal reached! Good job!");
+                    Console.WriteLine($"{stepsWalk - 10000} steps over the goal!");
+                    break;
+                }
+                step = Console.ReadLine();
+
+            }
+            if (step == "Going home")
+            {
+                step = Console.ReadLine();
+                stepsWalk += int.Parse(step);
+                // Find If wi reach the target when we go home
+                if (stepsWalk >= 10000)
+                {
+                    Console.WriteLine("Goal reached! Good job!");
+                    Console.WriteLine($"{stepsWalk - 10000} steps over the goal!");
+                }
+                else
+                {
+                    Console.WriteLine($"{10000 - stepsWalk} more steps to reach goal.");
+                }
+
+            }
+
+
+        }
+    }
+}
