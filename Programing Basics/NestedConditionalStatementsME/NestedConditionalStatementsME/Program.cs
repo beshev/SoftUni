2a0f49a578e5e5d027a80cc95b499c2e09d6f3b1
beshev <beshev1@gmail.com>
2020-12-06 08:43:52 +0200

    Added homework from course Programing Basics

---
+++ Programing Basics/NestedConditionalStatementsME/NestedConditionalStatementsME/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace NestedConditionalStatementsME
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 10; i >= 1; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            
+             
+        }
+    }
+}
