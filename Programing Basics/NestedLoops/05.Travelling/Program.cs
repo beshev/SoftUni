538f1119473e308fc8f5ae40801a520fd3e7a807
beshev <beshev1@gmail.com>
2020-12-07 09:24:09 +0200

    Added homework from course Programing Basics

---
+++ Programing Basics/NestedLoops/05.Travelling/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _05.Travelling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read Input:
+            string destination = Console.ReadLine();
+            double moneyHave = 0;
+            while (destination != "End")
+            {
+                // money needed for travel:
+                double moneyNeed = double.Parse(Console.ReadLine());
+                // Money save:
+                while (true)
+                {
+                    double moneySave = double.Parse(Console.ReadLine());
+                    moneyHave += moneySave;
+                    if (moneyHave >= moneyNeed)
+                    {
+                        moneyHave = 0;
+                        Console.WriteLine($"Going to {destination}!");
+                        break;
+                    }
+
+
+                }
+               
+                destination = Console.ReadLine();
+            }
+        }
+    }
+}
