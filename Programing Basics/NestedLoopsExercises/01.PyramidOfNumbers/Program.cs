b084e3b2a8a923be01248243b4b044bd71a4f1a9
beshev <beshev1@gmail.com>
2020-12-07 09:26:04 +0200

    Added homework from course Programing Basics

---
+++ Programing Basics/NestedLoopsExercises/01.PyramidOfNumbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int counter = 1;
+            int x1 = 1;
+
+
+            for (int i = 1; i <= n; i++)
+            {
+                for ( int y = 1; y <= counter; y++)
+                {
+                    Console.Write($"{x1} ");
+                    if (x1 == n)
+                    {
+                        return;
+                    }
+                    x1++;
+                   
+                }
+                counter++;
+                Console.WriteLine();
+            }
+
+            
+        }
+    }
+}
---
+++ Programing Basics/NestedLoopsExercises/05.PasswordGanerator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _05.PasswordGanerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int L = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int y = 1; y<= n; y++)
+                {
+                    
+                    for (char a = 'a'; a < 'a' + L; a++)
+                    {
+                        for (char a1 = 'a'; a1 < 'a' + L; a1++)
+                        {
+                            for (int j = 1; j <= n; j++)
+                            {
+                                if (j > i && j > y)
+                                {
+                                    Console.Write($"{i}{y}{a}{a1}{j} ");
+                                }
+                            }
+                        }
+                    }
+                
+                }
+
+            }
+
+
+
+        }
+    }
+}
