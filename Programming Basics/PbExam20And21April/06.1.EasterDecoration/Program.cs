0cced86164076c4e14cd21c49c95d246f774eb0e
beshev <beshev1@gmail.com>
2020-12-15 19:51:00 +0200

    Added homework from course Programming Basics

---
+++ Programming Basics/PbExam20And21April/06.1.EasterDecoration/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _06._1.EasterDecoration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numClient = int.Parse(Console.ReadLine());
+            double totalSum = 0;
+            double totalClient = 0;
+            while (numClient != 0)
+            {
+                double sumForOne = 0;
+                int counterBuy = 0;
+                string buyName = Console.ReadLine();
+                while (buyName != "Finish")
+                {
+                    switch (buyName)
+                    {
+                        case "basket":
+                            sumForOne += 1.50;
+                            counterBuy++;
+                            break;
+                        case "wreath":
+                            sumForOne += 3.80;
+                            counterBuy++;
+                            break;
+                        case "chocolate bunny":
+                            sumForOne += 7;
+                            counterBuy++;
+                            break;
+                    }
+                    buyName = Console.ReadLine();
+
+
+                }
+                if (counterBuy % 2 == 0)
+                {
+                    sumForOne = sumForOne - (sumForOne * 0.2);
+                Console.WriteLine($"You purchased {counterBuy} items for {sumForOne:f2} leva.");
+                }
+                else
+                {
+                    Console.WriteLine($"You purchased {counterBuy} items for {sumForOne:f2} leva.");
+                }
+                totalClient++;
+                totalSum += sumForOne;
+                numClient--;
+            }
+            Console.WriteLine($"Average bill per client is: {totalSum / totalClient:f2} leva.");
+        }
+    }
+}
---
+++ Programming Basics/PbExam20And21April/06.EasterCompetition/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _06.EasterCompetition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bread = int.Parse(Console.ReadLine());
+            int totalPoints = 0;
+            string nameBaker = "";
+
+            while (bread != 0)
+            {
+                int point = 0;
+                string bakerName = Console.ReadLine();
+                string bakerPoints = Console.ReadLine();
+                while (bakerPoints != "Stop")
+                {
+                    int evaluation = int.Parse(bakerPoints);
+                    point += evaluation;                    
+                    bakerPoints = Console.ReadLine();
+
+                }
+                Console.WriteLine($"{bakerName} has {point} points.");
+                if (point > totalPoints)
+                {
+                    totalPoints = point;
+                    nameBaker = bakerName;
+                    Console.WriteLine($"{nameBaker} is the new number 1!");
+                }
+                bread--;
+
+
+            }
+            Console.WriteLine($"{nameBaker} won competition with {totalPoints} points!");
+        }
+    }
+}
