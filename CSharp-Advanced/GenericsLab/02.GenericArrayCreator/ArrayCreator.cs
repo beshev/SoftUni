dda0cd3a31faa14e7733c9dd5a6bbdea65777b4e
beshev <beshev1@gmail.com>
2021-02-09 22:38:45 +0200

    Added homework from course CSharp-Advanced

---
+++ CSharp-Advanced/GenericsLab/02.GenericArrayCreator/ArrayCreator.cs
@@ -0,0 +1,16 @@
+﻿namespace GenericArrayCreator
+{
+    public class ArrayCreator
+    {
+
+        public static T[] Create<T>(int length, T item)
+        {
+            T[] arr = new T[length];
+            for (int i = 0; i < length; i++)
+            {
+                arr[i] = item;
+            }
+            return arr;
+        }
+    }
+}
