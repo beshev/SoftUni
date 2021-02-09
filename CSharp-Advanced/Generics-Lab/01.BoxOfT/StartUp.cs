5c31ac451d7432c029d388ec4c82a96b6d755094
beshev <beshev1@gmail.com>
2021-02-09 22:40:21 +0200

    Added homework from course CSharp-Advanced

---
+++ CSharp-Advanced/Generics-Lab/01.BoxOfT/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BoxOfT
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Box<int> box = new Box<int>();
+        }
+    }
+}
---
+++ CSharp-Advanced/Generics-Lab/GenericScale/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GenericScale
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            EqualityScale<int> resut = new EqualityScale<int>(5, 5);
+            Console.WriteLine(resut.AreEqual());
+        }
+    }
+}
