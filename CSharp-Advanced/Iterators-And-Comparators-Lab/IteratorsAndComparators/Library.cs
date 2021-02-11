65ecec656874631f9a231a45fecbbdc780c43cac
beshev <beshev1@gmail.com>
2021-02-11 21:25:17 +0200

    Added homework from course CSharp-Advanced

---
+++ CSharp-Advanced/Iterators-And-Comparators-Lab/IteratorsAndComparators/Library.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> books;
+
+        public Library(params Book[] books)
+        {
+            this.books = books.ToList();
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            books.Sort(new BookComparator());
+            return new LibraryIterator(books);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+
+        private class LibraryIterator : IEnumerator<Book>
+        {
+            private readonly List<Book> books;
+            private int index = -1;
+
+            public LibraryIterator(List<Book> books)
+            {
+                this.Reset();
+                this.books = books;
+            }
+
+            public Book Current => this.books[index];
+
+            object IEnumerator.Current => this.Current;
+
+            public void Dispose() { }
+
+            public bool MoveNext()
+            {
+                this.index++;
+                return this.index < this.books.Count;
+            }
+
+            public void Reset()
+            {
+                index = -1;
+            }
+        }
+    }
+}
