6637d571001cc56de5b6a198d71b7acf4b9d4b5d
beshev <beshev1@gmail.com>
2021-02-16 23:49:23 +0200

    Added homework from course CSharp-Advanced

---
+++ CSharp-Advanced/Preparation-For-Exam/C#AdvancedExam25October2020/01.Scheduling/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _01.Scheduling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> tasks = new Stack<int>
+                (Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse));
+            Queue<int> threads = new Queue<int>
+                (Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse));
+            int task = int.Parse(Console.ReadLine());
+            while (tasks.Peek() != task)
+            {
+                if (threads.Dequeue() >= tasks.Peek())
+                {
+                    tasks.Pop();
+                }
+            }
+            Console.WriteLine($"Thread with value {threads.Peek()} killed task {task}");
+            Console.WriteLine(string.Join(' ',threads));
+        }
+    }
+}
