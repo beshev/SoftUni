fe450157cddeb49762fe23523dc86b21aa5e3104
beshev <beshev1@gmail.com>
2021-02-17 23:45:33 +0200

    Added homework from course CSharp-Advanced

---
+++ CSharp-Advanced/Preparation-For-Exam/C#-Advanced-Retake-Exam-19-August-2020/03.VetClinic/VetClinic/Clinic.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VetClinic
+{
+    class Clinic
+    {
+        private List<Pet> data;
+
+        public Clinic(int capacity)
+        {
+            this.Capacity = capacity;
+            this.data = new List<Pet>();
+        }
+
+        public int Capacity { get; set; }
+
+        public int Count { get { return this.data.Count; } }
+
+        public void Add(Pet pet)
+        {
+            if (this.data.Count < this.Capacity)
+            {
+                this.data.Add(pet);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            Pet current = this.data.FirstOrDefault(p => p.Name == name);
+            if (current != null)
+            {
+                this.data.Remove(current);
+                return true;
+            }
+            return false;
+        }
+
+        public Pet GetPet(string name, string owner)
+        {
+            return this.data.FirstOrDefault(p => p.Name == name && p.Owner == owner);
+        }
+
+        public Pet GetOldestPet()
+        {
+            return this.data.OrderByDescending(p => p.Age).FirstOrDefault();
+        }
+
+        public string GetStatistics()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("The clinic has the following patients:");
+            foreach (var pet in this.data)
+            {
+                sb.AppendLine($"Pet {pet.Name} with owner: {pet.Owner}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+
+
+
+    }
+}
---
+++ CSharp-Advanced/Preparation-For-Exam/C#-Advanced-Retake-Exam-19-August-2020/03.VetClinic/VetClinic/Pet.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VetClinic
+{
+    class Pet
+    {
+        public Pet(string name, int age, string owner)
+        {
+            Name = name;
+            Age = age;
+            Owner = owner;
+        }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Owner { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {Name} Age: {Age} Owner: {Owner}";
+        }
+
+    }
+}
