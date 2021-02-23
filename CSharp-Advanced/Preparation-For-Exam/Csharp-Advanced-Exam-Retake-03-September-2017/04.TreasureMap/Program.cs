9a6c2fa6914dc5561a952edcd72525506b917d99
beshev <beshev1@gmail.com>
2021-02-23 21:48:19 +0200

    Added homework from course CSharp-Advanced

---
+++ CSharp-Advanced/Preparation-For-Exam/Csharp-Advanced-Exam-Retake-03-September-2017/04.TreasureMap/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace _04.TreasureMap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                MatchCollection matches = Regex.Matches(input,
+                    @"(!|#)[^!#]*?\b(?<street>[A-Za-z]{4})\b[^!#]*(?<!\d)(?<number>\d{3})-(?<password>\d{6}|\d{4})(?!\d)[^!#]*?(?!\1)(#|!)");
+                Match match = matches[matches.Count / 2];
+                Console.WriteLine($"Go to str. {match.Groups["street"]} {match.Groups["number"]}." +
+                    $" Secret pass: {match.Groups["password"]}.");
+            }
+        }
+    }
+}
---
+++ CSharp-Advanced/Preparation-For-Exam/Csharp-Advanced-Exam-Retake-03-September-2017/SandBox/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _07.EqualArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = Console.ReadLine()
+               .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+               .Select(int.Parse)
+               .ToArray();
+
+            int[] array2 = Console.ReadLine()
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray(); 
+            int sumOfArrays = 0;
+            for (int i = 0; i < array2.Length; i++)
+            {
+                if (array1[i] == array2 [i])
+                {
+                    sumOfArrays += array1[i];
+                }
+                else
+                {
+                    Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                    return;
+                }
+            }
+            Console.WriteLine($"Arrays are identical. Sum: {sumOfArrays}");
+        }
+    }
+}
