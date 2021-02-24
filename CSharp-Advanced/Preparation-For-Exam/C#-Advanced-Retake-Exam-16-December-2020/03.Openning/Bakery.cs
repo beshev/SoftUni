249aaeb5087cee8ba630a6ae6827a69de272ab7c
beshev <beshev1@gmail.com>
2021-02-24 19:43:53 +0200

    Added homework from course CSharp-Advanced

---
+++ CSharp-Advanced/Preparation-For-Exam/C#-Advanced-Retake-Exam-16-December-2020/03.Openning/Bakery.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BakeryOpenning
+{
+    class Bakery
+    {
+        private List<Employee> data;
+
+        public Bakery(string name, int capacity)
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+            this.data = new List<Employee>();
+        }
+
+        public string Name { get; set; }
+
+        public int Capacity { get; set; }
+
+        public int Count
+        {
+            get
+            {
+                return data.Count;
+            }
+        }
+
+        public void Add(Employee employee)
+        {
+            if (this.data.Count < Capacity)
+            {
+                this.data.Add(employee);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            Employee current = this.data.FirstOrDefault(e => e.Name == name);
+            if (current != null)
+            {
+                this.data.Remove(current);
+                return true;
+            }
+            return false;
+        }
+
+        public Employee GetOldestEmployee()
+        {
+            return this.data.OrderByDescending(x => x.Age).FirstOrDefault();
+        }
+
+        public Employee GetEmployee(string name)
+        {
+            return this.data.FirstOrDefault(e => e.Name == name);
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Employees working at Bakery {this.Name}:");
+            foreach (var employy in data)
+            {
+                sb.AppendLine(employy.ToString());
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
--- CSharp-Advanced/Preparation-For-Exam/C#-Advanced-Retake-Exam-16-December-2020/03.Openning/Employee.cs
+++ CSharp-Advanced/Preparation-For-Exam/C#-Advanced-Retake-Exam-16-December-2020/03.Openning/Employee.cs
@@ -6,5 +6,22 @@ namespace BakeryOpenning
 {
     class Employee
     {
+        public Employee(string name,int age ,string country)
+        {
+            this.Age = age;
+            this.Name = name;
+            this.Country = country;
+        }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Country { get; set; }
+
+        public override string ToString()
+        {
+            return $"Employee: {Name}, {Age} ({Country})";
+        }
     }
 }
