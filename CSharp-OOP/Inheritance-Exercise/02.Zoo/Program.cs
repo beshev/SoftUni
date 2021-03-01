ab4ce6b340b621aafffd6f9adc8650f91f6ea064
beshev <beshev1@gmail.com>
2021-03-01 23:42:10 +0200

    Added exercise "02.Zoo" from course "CSharp-OOP"

---
+++ CSharp-OOP/Inheritance-Exercise/02.Zoo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Zoo
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal("Bug");
+            Bear bear = new Bear("GOsho");
+        }
+    }
+}
