8565982654e1da86b09f455c8e6c707edc702ff1
beshev <beshev1@gmail.com>
2021-03-01 23:43:29 +0200

    Added exercise "03.PlayersAndMonsters" from course "CSharp-OOP"

---
+++ CSharp-OOP/Inheritance-Exercise/03. PlayersAndMonsters/Knight.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters
+{
+    class Knight : Hero
+    {
+        public Knight(string username, int age) : base(username, age)
+        {
+
+        }
+    }
+}
---
+++ CSharp-OOP/Inheritance-Exercise/03. PlayersAndMonsters/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PlayersAndMonsters
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            BladeKnight bladeKnight = new BladeKnight("Gosho",80);
+            DarkWizard darkWizard = new DarkWizard("Tosho",100);
+            Console.WriteLine(bladeKnight);
+            Console.WriteLine(darkWizard);
+
+        }
+    }
+}
