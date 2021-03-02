f0cf7dcfce6034e5408e68d0cfa96861858311c1
beshev <beshev1@gmail.com>
2021-03-02 23:36:38 +0200

    Added exercise "05.Restaurant" from course "CSharp-OOP"

---
+++ CSharp-OOP/Inheritance-Exercise/05.Restaurant/Cake.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Cake : Dessert
+    {
+        private const double grams = 250;
+        private const double calories = 1000;
+        private const decimal cakePrice = 5m;
+
+
+        public Cake(string name) : base(name, cakePrice, grams, calories)
+        {
+
+        }
+
+        public decimal CakePrice { get; set; }
+    }
+}
