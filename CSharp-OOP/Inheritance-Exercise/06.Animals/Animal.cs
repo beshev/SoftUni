60913b0b6519ee7b50217405a8a0e31142911fb5
beshev <beshev1@gmail.com>
2021-03-02 23:37:25 +0200

    Added exercise "06.Animals" from course "CSharp-OOP"

---
+++ CSharp-OOP/Inheritance-Exercise/06.Animals/Animal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Animal
+    {
+        public Animal(string name, int age, string gender)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Gender = gender;
+        }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Gender { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} {Age} {Gender}";
+        }
+
+        public virtual string ProduceSound() { throw new NullReferenceException("No Animal");}
+
+        public string Type()
+        {
+            return this.GetType().Name;
+        }
+    }
+}
