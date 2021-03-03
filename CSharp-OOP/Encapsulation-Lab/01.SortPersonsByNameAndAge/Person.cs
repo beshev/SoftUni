ece8143017c3281a7f8c478bcdfa0ca352c68adb
beshev <beshev1@gmail.com>
2021-03-03 23:39:54 +0200

    Added exercise "01.SortPersonsByNameAndAge" from course "CSharp-OOP"

---
+++ CSharp-OOP/Encapsulation-Lab/01.SortPersonsByNameAndAge/Person.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        private string firstName;
+
+        private string lastName;
+
+        private int age;
+
+        private decimal salary;
+
+        public Person(string firstName, string lastName, int age,decimal salary)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Salary = salary;
+        }
+
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+                }
+                firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+                }
+                lastName = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Age cannot be zero or a negative integer!");
+                }
+                age = value;
+            }
+        }
+
+        public decimal Salary
+        {
+            get
+            {
+                return salary;
+            }
+            set
+            {
+                if (value < 460)
+                {
+                    throw new ArgumentException("Salary cannot be less than 460 leva!");
+                }
+                salary = value;
+            }
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (Age < 30)
+            {
+                Salary += Salary * percentage / 200;
+            }
+            else
+            {
+                Salary += Salary * percentage / 100;
+            }
+        }
+
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} receives {Salary:f2} leva.";
+        }
+    }
+}
