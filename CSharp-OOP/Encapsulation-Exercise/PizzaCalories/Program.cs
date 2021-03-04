cf84ec298d28db50645d69c0d98df5ad5ec9121b
beshev <beshev1@gmail.com>
2021-03-04 23:40:16 +0200

    Added exercise "PizzaCalories" from course "CSharp-OOP"

---
+++ CSharp-OOP/Encapsulation-Exercise/PizzaCalories/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PizzaCalories
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string[] input = Console.ReadLine().Split(' ');
+                string[] doughInfo = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                Pizza pizza = new Pizza(input[1]);
+                pizza.AddDough(new Dough(doughInfo[1], doughInfo[2], double.Parse(doughInfo[3])));
+
+                string topping = Console.ReadLine();
+                while (topping != "END")
+                {
+                    string[] toppingInfo = topping.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                    pizza.AddToppings(new Topping(toppingInfo[1], double.Parse(toppingInfo[2])));
+                    topping = Console.ReadLine();
+                }
+                Console.WriteLine($"{pizza.Name} - {pizza.TotalCalories:f2} Calories.");
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
