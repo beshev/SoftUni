1d0decb02e407c77b506256994f3e34f53187832
beshev <beshev1@gmail.com>
2021-03-08 21:36:32 +0200

    Add InterfacesAndAbstractionExercise

---
+++ CSharp-OOP/Interfaces-And-Abstraction-Exercise/01.DefineAnInterfaceIPerson/Citizen.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonInfo
+{
+    class Citizen : IPerson
+    {
+        public Citizen(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
