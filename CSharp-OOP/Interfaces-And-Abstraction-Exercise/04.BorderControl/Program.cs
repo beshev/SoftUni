c96578bafa4f57680507e3de7324dc189816b5c1
beshev <beshev1@gmail.com>
2021-03-09 21:30:07 +0200

    Add InterfacesAndAbstractionExercise

---
+++ CSharp-OOP/Interfaces-And-Abstraction-Exercise/04.BorderControl/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using _04.BorderControl.Core;
+
+namespace _04.BorderControl
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Engine engine = new Engine();
+            engine.Run();
+        }
+    }
+}
