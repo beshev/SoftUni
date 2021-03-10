a2e577e5f50565f1dcd64533d6e1eb5de5bd834b
beshev <beshev1@gmail.com>
2021-03-10 21:44:29 +0200

    Add WorkingWithInterfacesAndAbstractionExercise

---
+++ CSharp-OOP/Interfaces-And-Abstraction-Exercise/08.CollectionHierarchy/Models/AddRemoveCollection.cs
@@ -0,0 +1,28 @@
+﻿using _08.CollectionHierarchy.Models.Contracts;
+using System.Collections.Generic;
+
+namespace _08.CollectionHierarchy.Models
+{
+    class AddRemoveCollection : IRemovable
+    {
+        private LinkedList<string> list;
+
+        public AddRemoveCollection()
+        {
+            list = new LinkedList<string>();
+        }
+
+        public int Add(string element)
+        {
+            list.AddFirst(element);
+            return 0;
+        }
+
+        public string Remove()
+        {
+            string lastElement = list.Last.Value;
+            list.RemoveLast();
+            return lastElement;
+        }
+    }
+}
---
+++ CSharp-OOP/Interfaces-And-Abstraction-Exercise/08.CollectionHierarchy/Models/Contracts/IRemovable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _08.CollectionHierarchy.Models.Contracts
+{
+    interface IRemovable : IAddable
+    {
+        public string Remove();
+    }
+}
