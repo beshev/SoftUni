c0359904410b6e190620329cc40abe2406c1d310
beshev <beshev1@gmail.com>
2021-03-11 21:37:41 +0200

    Add WorkingWithInterfacesAndAbstractionExercise

---
+++ CSharp-OOP/Interfaces-And-Abstraction-Exercise/09.ExplicitInterfaces/Program.cs
@@ -0,0 +1,13 @@
+﻿using _09.ExplicitInterfaces.Core;
+
+namespace _09.ExplicitInterfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Engine engine = new Engine();
+            engine.Run();
+        }
+    }
+}
