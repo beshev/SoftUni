5c3861e709df195a37716593016e8e1a578e7c8a
beshev <beshev1@gmail.com>
2021-03-11 21:38:44 +0200

    Add WorkingWithInterfacesAndAbstractionExercise

---
+++ CSharp-OOP/Interfaces-And-Abstraction-Exercise/SandBox/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SandBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Soldier> soldiers = new List<Soldier>();
+            soldiers.Add(new Engineer("asd","asd",2m,2));
+            foreach (var item in soldiers)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+
+    interface ISoldier
+    {
+        public string Name { get; set; }
+
+        public string ID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string ToString();
+    }
+
+    class Soldier : ISoldier
+    {
+        public Soldier(string name,string age,decimal salary)
+        {
+
+        }
+        public string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string ID { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string FirstName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public override string ToString()
+        {
+            return "In the soldier";
+        }
+    }
+
+    class Private : Soldier
+    {
+        public Private(string name, string age, decimal salary,int rate) : base(name, age, salary)
+        {
+        }
+
+        public override string ToString()
+        {
+            return "In the Private";
+        }
+    }
+    class Engineer : Private
+    {
+        public Engineer(string name, string age, decimal salary, int rate) : base(name, age, salary, rate)
+        {
+        }
+
+        public override string ToString()
+        {
+            return "In the Engineer";
+        }
+    }
+}
