3188d6fae0369b9ed3318a0ffdd204d6c6b51de0
beshev <beshev1@gmail.com>
2021-03-15 23:39:02 +0200

    Add PolymorphismExercises

---
+++ CSharp-OOP/Polymorphism-Exercises/03.Raiding/Paladin.cs
@@ -0,0 +1,17 @@
+﻿using _03.Raiding.Common;
+
+namespace _03.Raiding
+{
+    public class Paladin : BaseHero
+    {
+        private const int POWER = 100;
+
+        public Paladin(string name) : base(name, POWER)
+        { }
+
+        public override string CastAbility()
+        {
+            return string.Format(GlobalConst.HEALING_INFO, this.GetType().Name, this.Name, this.Power);
+        }
+    }
+}
