6d566a717e8f590638c94e5eaa92a77a716e9fed
beshev <beshev1@gmail.com>
2021-03-21 20:26:25 +0200

    Add ReflectionLab

---
+++ CSharp-OOP/Reflection-And-Attributes-Lab/Stealer/Core/Engine.cs
@@ -0,0 +1,17 @@
+﻿using Stealer.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stealer.Core
+{
+  public class Engine
+    {
+        public void Run()
+        {
+            Spy spy = new Spy();
+            string resut = spy.CollectGettesAndSetters("Stealer.Hacker");
+            Console.WriteLine(resut);
+        }
+    }
+}
---
+++ CSharp-OOP/Reflection-And-Attributes-Lab/Stealer/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using Stealer.Core;
+using System;
+
+namespace Stealer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Engine engine = new Engine();
+            engine.Run();
+        }
+    }
+}
