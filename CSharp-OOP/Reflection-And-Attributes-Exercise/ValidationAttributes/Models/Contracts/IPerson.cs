c8d3b4fcbf4beb9ccd58ac71355bb19749a847d0
beshev <beshev1@gmail.com>
2021-03-23 20:15:48 +0200

    Added ReflectionExercise

---
+++ CSharp-OOP/Reflection-And-Attributes-Exercise/ValidationAttributes/Models/Contracts/IPerson.cs
@@ -0,0 +1,10 @@
+﻿namespace ValidationAttributes.Models.Contracts
+{
+    public interface IPerson
+    {
+        public string FullName { get; }
+
+        public int Age { get; }
+
+    }
+}
---
+++ CSharp-OOP/Reflection-And-Attributes-Exercise/ValidationAttributes/Models/Validator.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using System.Reflection;
+using ValidationAttributes.Attributes;
+
+namespace ValidationAttributes.Models
+{
+    public static class Validator
+    {
+        public static bool IsValid(object obj)
+        {
+            PropertyInfo[] allProp = obj.GetType().GetProperties();
+            foreach (var prop in allProp)
+            {
+                var allAttr = prop
+                    .GetCustomAttributes(true)
+                    .Where(a => a is MyValidationAttribute)
+                    .Cast<MyValidationAttribute>()
+                    .ToArray();
+
+
+                foreach (var attr in allAttr)
+                {
+                    if (attr.IsValid(prop.GetValue(obj)) == false)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
