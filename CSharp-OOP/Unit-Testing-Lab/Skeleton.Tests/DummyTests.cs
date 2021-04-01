b4cf0e063390d3a74d1605aae270849dd6250172
beshev <beshev1@gmail.com>
2021-04-01 23:42:53 +0300

    Added UnitTesting-SkeletonLab

---
+++ CSharp-OOP/Unit-Testing-Lab/Skeleton.Tests/DummyTests.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+using System;
+
+[TestFixture]
+public class DummyTests
+{
+    [Test]
+    [TestCase(100, 100, 10)]
+    public void DummyShouldLosesHealthIfAttacked(int health, int experience, int attackPoint)
+    {
+        // Arrange
+        Dummy dummy = new Dummy(health, experience);
+
+        // Act
+        int expectedHealth = dummy.Health - attackPoint;
+
+        dummy.TakeAttack(attackPoint);
+        int actualHealth = dummy.Health;
+
+        // Assert
+        Assert.AreEqual(expectedHealth, actualHealth);
+    }
+
+    [Test]
+    [TestCase(0, 100)]
+    public void DeadDummyShouldThrowsExceptionIfAttacked(int health, int experience)
+    {
+        // Arrange
+        Dummy dummy = new Dummy(health, experience);
+
+        // Act - Assert
+        Assert.Throws<InvalidOperationException>(() => dummy.TakeAttack(10));
+    }
+
+    [Test]
+    [TestCase(0, 100)]
+    public void DeadDummyShouldCanGiveXP(int health, int experience)
+    {
+        // Arrange
+        Dummy dummy = new Dummy(health, experience);
+
+        // Act 
+        int expectedValue = experience;
+        int actualValue = dummy.GiveExperience();
+
+        // Assert
+        Assert.AreEqual(expectedValue, actualValue);
+    }
+
+    [Test]
+    [TestCase(100, 100)]
+    public void AliveDummyCantGiveXP(int health, int experience)
+    {
+        // Arrange
+        Dummy dummy = new Dummy(health, experience);
+
+        // Act - Assert
+        Assert.Throws<InvalidOperationException>(() => dummy.GiveExperience());
+    }
+}
