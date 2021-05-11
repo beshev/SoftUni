1a431cb9c11c639030ffba6c0902ee7fa496bff8
beshev <beshev1@gmail.com>
2021-05-11 23:36:00 +0300

    Added homework

---
+++ CSharp-DB/Basic/Homeworks/01-Database-Introduction/21.BasicSelectSomeFields.sql
@@ -0,0 +1,9 @@
+SELECT [Name] FROM Towns
+
+SELECT [Name] FROM Departments
+
+SELECT FirstName,
+LastName,
+JobTitle,
+Salary 
+FROM Employees
---
+++ CSharp-DB/Basic/Homeworks/01-Database-Introduction/23.DecreaseTaxRate.sql
@@ -0,0 +1,4 @@
+UPDATE Payments
+SET TaxRate *= 0.97
+
+SELECT TaxRate FROM Payments
