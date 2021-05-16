c60da7c7385b368c190f472fd3f298d92c727a13
beshev <beshev1@gmail.com>
2021-05-16 13:10:42 +0300

    Added Homework

---
+++ CSharp-DB/Database-Basic/Homeworks/05-Subqueries-And-Joins/05.EmployeesWithoutProjects.sql
@@ -0,0 +1,5 @@
+SELECT TOP(3) e.EmployeeID,e.FirstName 
+	FROM Employees AS e
+	LEFT JOIN EmployeesProjects AS ep ON ep.EmployeeID = e.EmployeeID
+ WHERE ep.EmployeeID IS NULL
+ ORDER BY e.EmployeeID
