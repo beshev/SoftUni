b9e6673fc920b378926db3e6235a8cf690e9717a
beshev <beshev1@gmail.com>
2021-06-11 11:05:05 +0300

    Added Homework

---
+++ CSharp-DB/Databases-Advanced/Homeworks/C#AutoMappingObjects/FastFood.Core/MappingConfiguration/FastFoodProfile.cs
@@ -0,0 +1,62 @@
+﻿namespace FastFood.Core.MappingConfiguration
+{
+    using AutoMapper;
+    using FastFood.Core.ViewModels.Categories;
+    using FastFood.Models;
+    using ViewModels.Positions;
+    using ViewModels.Employees;
+    using FastFood.Core.ViewModels.Items;
+    using FastFood.Core.ViewModels.Orders;
+    using System;
+    using System.Globalization;
+
+    public class FastFoodProfile : Profile
+    {
+        public FastFoodProfile()
+        {
+            //Positions
+            this.CreateMap<CreatePositionInputModel, Position>()
+                .ForMember(x => x.Name, y => y.MapFrom(s => s.PositionName));
+
+            this.CreateMap<Position, PositionsAllViewModel>()
+                .ForMember(x => x.Name, y => y.MapFrom(s => s.Name));
+
+            //Categories
+            this.CreateMap<CreateCategoryInputModel, Category>()
+                .ForMember(x => x.Name, y => y.MapFrom(s => s.CategoryName));
+
+            this.CreateMap<Category, CategoryAllViewModel>();
+
+            //Employyes
+            this.CreateMap<Position, RegisterEmployeeViewModel>()
+                .ForMember(x => x.PositionId, y => y.MapFrom(s => s.Id));
+
+            this.CreateMap<RegisterEmployeeInputModel, Employee>();
+
+            this.CreateMap<Employee, EmployeesAllViewModel>()
+                .ForMember(x => x.Position, y => y.MapFrom(s => s.Position.Name));
+
+            //Item
+            this.CreateMap<Category, CreateItemViewModel>()
+                .ForMember(x => x.CategoryId, y => y.MapFrom(s => s.Id));
+
+            this.CreateMap<Item, ItemsAllViewModels>()
+                .ForMember(x => x.Category, y => y.MapFrom(s => s.Category.Name));
+
+            this.CreateMap<CreateItemInputModel, Item>();
+
+            //Order
+            this.CreateMap<CreateOrderInputModel, Order>()
+                .ForMember(x => x.DateTime, y => y.MapFrom(s => DateTime.UtcNow));
+
+            this.CreateMap<Order, OrderAllViewModel>()
+                .ForMember(x => x.DateTime, y => y.MapFrom(s => s.DateTime.ToString("dd-MM-yyyy",CultureInfo.InvariantCulture)))
+                .ForMember(x => x.Employee, y => y.MapFrom(s => s.Employee.Name))
+                .ForMember(x => x.OrderId, y => y.MapFrom(s => s.Id));
+
+
+
+
+        }
+    }
+}
