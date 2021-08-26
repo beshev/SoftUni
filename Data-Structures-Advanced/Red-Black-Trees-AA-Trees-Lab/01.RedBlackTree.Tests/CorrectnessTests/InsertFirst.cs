33a2a9953295e468a030105f0cd50a42904d1c81
Dimitar Beshev <beshev1@gmail.com>
2021-08-26 20:52:42 +0300

    Added homework

---
+++ Data-Structures-Advanced/Red-Black-Trees-AA-Trees-Lab/01.RedBlackTree.Tests/CorrectnessTests/InsertFirst.cs
@@ -0,0 +1,25 @@
+﻿using _01.RedBlackTree;
+using NUnit.Framework;
+
+[TestFixture]
+class InsertFirst
+{
+    [Test]
+    public void Insert_SingleElement_ShouldIncreaseCount()
+    {
+        RedBlackTree<int> rbt = new RedBlackTree<int>();
+        rbt.Insert(5);
+
+        Assert.AreEqual(1, rbt.Count());
+    }
+
+
+    [Test]
+    public void IMustDeleteThisTest()
+    {
+        RedBlackTree<int> rbt = new RedBlackTree<int>();
+        //rbt.Insert(5);
+        rbt.Insert(3);
+        rbt.Insert(4);
+    }
+}
