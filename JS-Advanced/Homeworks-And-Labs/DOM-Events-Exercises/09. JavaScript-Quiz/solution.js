f24802a380e8f6f6b4140d3f0560d8c2c8af6a0b
Dimitar Beshev <beshev1@gmail.com>
2021-09-22 10:06:31 +0300

    Finished quiz exercise

--- JS-Advanced/Homeworks-And-Labs/DOM-Events-Exercises/09. JavaScript-Quiz/solution.js
+++ JS-Advanced/Homeworks-And-Labs/DOM-Events-Exercises/09. JavaScript-Quiz/solution.js
@@ -1,3 +1,39 @@
 function solve() {
-  //TODO...
+  let sectionsElements = Array.from(document.querySelectorAll('section'));
+  let currectAnswers = [
+    'onclick',
+    'JSON.stringify()',
+    'A programming API for HTML and XML documents',
+  ];
+  let rightAnswers = 0;
+
+  nextSection(0);
+
+    function nextSection(index) {
+      let sectionElement = sectionsElements[index];
+      sectionElement.style.display = 'block';
+
+      Array.from(sectionElement.querySelectorAll('ul li div p.answer-text'))
+      .forEach(answer => {
+          answer.addEventListener('click', (e) => {
+            if (currectAnswers.includes(e.target.textContent)) rightAnswers++;
+            sectionElement.style.display = 'none';
+
+            if (index == sectionsElements.length - 1) {
+                let resultElement = document.querySelector('#results li h1');
+
+                if (rightAnswers == 3) {
+                  resultElement.textContent = 'You are recognized as top JavaScript fan!'
+                } else {
+                  resultElement.textContent = `You have ${rightAnswers} right answers`;
+                }
+
+                resultElement.parentElement.parentElement.style.display = 'block';
+
+              return;
+            }
+            nextSection(index + 1);
+        });
+      });
+    }
 }
