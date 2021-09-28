a4298fca6f2769b10a7aac1ed0b511a6f3df4249
Dimitar Beshev <beshev1@gmail.com>
2021-09-28 09:28:20 +0300

    Solved 6th & 7th exercise

--- JS-Advanced/Homeworks-And-Labs/Unit-Testing-And-Error-Handling-Lab/test/06.RGBToHex.test.js
+++ JS-Advanced/Homeworks-And-Labs/Unit-Testing-And-Error-Handling-Lab/test/06.RGBToHex.test.js
@@ -3,39 +3,70 @@ const assert = require('chai').assert;
 
 describe('rgbToHexColor tests',  () => {
     it('Should return undefined with negative numbers', () => {
-        assert.equal(rgbToHexColor(-1,2,2),undefined)
-        assert.equal(rgbToHexColor(2,-1,2),undefined)
-        assert.equal(rgbToHexColor(2,2,-2),undefined)
+        assert.strictEqual(rgbToHexColor(-50,2,2),undefined)
+        assert.strictEqual(rgbToHexColor(2,-23,2),undefined)
+        assert.strictEqual(rgbToHexColor(2,2,-2),undefined)
     })
 
     it('Should return undefined with number more than 255', () => {
-        assert.equal(rgbToHexColor(256,2,2),undefined)
-        assert.equal(rgbToHexColor(2,1000,2),undefined)
-        assert.equal(rgbToHexColor(2,2,600),undefined)
+        assert.strictEqual(rgbToHexColor(256,2,2),undefined)
+        assert.strictEqual(rgbToHexColor(2,1000,2),undefined)
+        assert.strictEqual(rgbToHexColor(2,2,600),undefined)
     })
 
     it('Should return undefined with float numbers', () => {
-        assert.equal(rgbToHexColor(2.56,2,2),undefined)
-        assert.equal(rgbToHexColor(2,20.35,2),undefined)
-        assert.equal(rgbToHexColor(2,2,156.23),undefined)
+        assert.strictEqual(rgbToHexColor(2.56,2,2),undefined)
+        assert.strictEqual(rgbToHexColor(2,20.35,2),undefined)
+        assert.strictEqual(rgbToHexColor(2,2,156.23),undefined)
     })
 
     it('Should return undefined with invalid values types', () => {
-        assert.equal(rgbToHexColor(null,2,2),undefined)
-        assert.equal(rgbToHexColor(2,undefined,2),undefined)
-        assert.equal(rgbToHexColor(2,2,'yes baby'),undefined)
+        assert.strictEqual(rgbToHexColor([],2,2),undefined)
+        assert.strictEqual(rgbToHexColor(2,{},2),undefined)
+        assert.strictEqual(rgbToHexColor(2,2,null),undefined)
     })
 
-    it('Should work correct', () => {
-        let expected = '#010203';
-        let actual = rgbToHexColor(1,2,3);
+    it('Should return black', () => {
+        let red = 0;
+        let green = 0;
+        let blue = 0;
+
+        let expected = "#" +
+                    ("0" + red.toString(16).toUpperCase()).slice(-2) +
+                    ("0" + green.toString(16).toUpperCase()).slice(-2) +
+                    ("0" + blue.toString(16).toUpperCase()).slice(-2);
+
+        let actual = rgbToHexColor(0,0,0);
         
-        assert.equal(actual,expected);
+        assert.strictEqual(actual,expected);
+    })
+
+    
+    it('Should return white', () => {
+        let red = 255;
+        let green = 255;
+        let blue = 255;
+
+        let expected = "#" +
+                    ("0" + red.toString(16).toUpperCase()).slice(-2) +
+                    ("0" + green.toString(16).toUpperCase()).slice(-2) +
+                    ("0" + blue.toString(16).toUpperCase()).slice(-2);
+
+        let actual = rgbToHexColor(255,255,255);
+        
+        assert.strictEqual(actual,expected);
     })
 
     it('Should work uncorrect', () => {
-        let expected = '#010204';
-        let actual = rgbToHexColor(1,2,3);
+        let red = 1;
+        let green = 2;
+        let blue = 20;
+        let expected = "#" +
+                    ("0" + red.toString(16).toUpperCase()).slice(-2) +
+                    ("0" + green.toString(16).toUpperCase()).slice(-2) +
+                    ("0" + blue.toString(16).toUpperCase()).slice(-2);
+
+        let actual = rgbToHexColor(2,2,2);
         
         assert.notEqual(actual,expected);
     })
