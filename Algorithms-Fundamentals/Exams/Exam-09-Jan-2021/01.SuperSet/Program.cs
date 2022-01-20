7e3845200671032a844811b3b9b3aae91eba7dcd
Dimitar Beshev <beshev1@gmail.com>
2022-01-20 15:25:36 +0200

    Added homework

---
+++ Algorithms-Fundamentals/Exams/Exam-09-Jan-2021/01.SuperSet/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _01.SuperSet
+{
+    internal class Program
+    {
+        private static string[] arr;
+        private static string[] combinations;
+
+        static void Main(string[] args)
+        {
+            arr = Console.ReadLine()
+               .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                combinations = new string[i];
+
+                Combinations();
+            }
+
+
+            Console.WriteLine(string.Join(' ', arr));
+        }
+
+        private static void Combinations(int index = 0, int start = 0)
+        {
+            if (index >= combinations.Length)
+            {
+                Console.WriteLine(string.Join(" ", combinations));
+                return;
+            }
+
+
+            for (int i = start; i < arr.Length; i++)
+            {
+                combinations[index] = arr[i];
+                Combinations(index + 1, i + 1);
+            }
+        }
+    }
+}
---
+++ Algorithms-Fundamentals/Exams/Exam-09-Jan-2021/02.Paths/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.Paths
+{
+    internal class Program
+    {
+        private static List<int>[] graph;
+        private static bool[] visited;
+
+        static void Main(string[] args)
+        {
+            int nodes = int.Parse(Console.ReadLine());
+
+            graph = new List<int>[nodes];
+
+            for (int i = 0; i < nodes; i++)
+            {
+                graph[i] = new List<int>();
+            }
+
+            visited = new bool[nodes];
+
+            for (int node = 0; node < nodes - 1; node++)
+            {
+                string rowData = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(rowData))
+                {
+                    continue;
+                }
+
+                var children = rowData
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToList();
+
+                graph[node] = children;
+            }
+
+            for (int node = 0; node < graph.Length - 1; node++)
+            {
+                DFS(node);
+            }
+
+        }
+
+        private static void DFS(int node)
+        {
+            visited[node] = true;
+
+            if (node == graph.Length - 1)
+            {
+                PrintPath();
+                return;
+            }
+
+            foreach (var child in graph[node])
+            {
+                DFS(child);
+            }
+
+            visited[node] = false;
+        }
+
+        private static void PrintPath()
+        {
+            var path = new List<int>();
+
+            for (int i = 0; i < visited.Length; i++)
+            {
+                if (visited[i])
+                {
+                    path.Add(i);
+                }
+            }
+
+            Console.WriteLine(String.Join(' ', path));
+        }
+    }
+}
