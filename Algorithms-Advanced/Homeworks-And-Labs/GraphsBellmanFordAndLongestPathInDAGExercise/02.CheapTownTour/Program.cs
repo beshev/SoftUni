31d1b110ec62e2e48e3b189694c0e07566e4c08f
Dimitar Beshev <beshev1@gmail.com>
2022-02-08 14:40:24 +0200

    Added homework

---
+++ Algorithms-Advanced/Homeworks-And-Labs/GraphsBellmanFordAndLongestPathInDAGExercise/02.CheapTownTour/Program.cs
@@ -0,0 +1,81 @@
+﻿namespace _02.CheapTownTour
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Edge
+    {
+        public int First { get; set; }
+
+        public int Second { get; set; }
+
+        public double Weight { get; set; }
+    }
+
+    internal class Program
+    {
+        static void Main()
+        {
+            var nodesCount = int.Parse(Console.ReadLine());
+            var edgesCount = int.Parse(Console.ReadLine());
+
+            var edges = new List<Edge>();
+
+            for (int i = 0; i < edgesCount; i++)
+            {
+                var edgeData = Console.ReadLine()
+                    .Split(" - ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                edges.Add(new Edge
+                {
+                    First = edgeData[0],
+                    Second = edgeData[1],
+                    Weight = edgeData[2],
+                });
+            }
+
+
+            var parent = new int[nodesCount];
+
+            for (int i = 0; i < parent.Length; i++)
+            {
+                parent[i] = i;
+            }
+
+            var sortedEges = edges.OrderBy(e => e.Weight).ToArray();
+
+            var sum = 0d;
+
+            foreach (var edge in sortedEges)
+            {
+                var firstRoot = FindRoot(edge.First, parent);
+                var secondRoot = FindRoot(edge.Second, parent);
+
+                if (firstRoot == secondRoot)
+                {
+                    continue;
+                }
+
+                parent[firstRoot] = secondRoot;
+
+                sum += edge.Weight;
+            }
+
+            Console.WriteLine($"Total cost: {sum}");
+
+        }
+
+        private static int FindRoot(int node, int[] parent)
+        {
+            while (parent[node] != node)
+            {
+                node = parent[node];
+            }
+
+            return node;
+        }
+    }
+}
